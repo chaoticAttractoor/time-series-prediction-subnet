1764a3d5bdc4e4265eacc278e0f4226319d6e2ca
chaoticAttractoor <115046984+chaoticAttractoor@users.noreply.github.com>
2024-02-26 14:30:57 +1000

    Update miner.py

--- neurons/miner.py
+++ neurons/miner.py
@@ -186,7 +186,7 @@ def update_predictions(
                     )
 
                     # TODO: Improve validators to allow multiple features in predictions
-                    predicted_closes = prediction_array.tolist()
+                    predicted_closes = prediction_array
 
                     bt.logging.debug(f"predicted closes [{predicted_closes}]")
 
@@ -297,7 +297,7 @@ def update_predictions_adaptive(
                     )
 
                     # TODO: Improve validators to allow multiple features in predictions
-                    predicted_closes = prediction_array.tolist()
+                    predicted_closes = prediction_array
 
                     bt.logging.debug(f"predicted closes [{predicted_closes}]")
 
